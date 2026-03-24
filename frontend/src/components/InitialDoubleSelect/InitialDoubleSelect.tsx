16c08bc111ccad96c2898b2260bd1b861e4166f6
Gabriel Neves Ferreira <gabrielnevesferreira0@gmail.com>
2026-03-24 12:33:59 -0300

    Link na Div Home de Entregas Internacionais, Algumas variavies de Cores criadas e aplicadas para o modo Alto Contraste

--- frontend/src/components/InitialDoubleSelect/InitialDoubleSelect.tsx
+++ frontend/src/components/InitialDoubleSelect/InitialDoubleSelect.tsx
@@ -1,13 +1,17 @@
 import style from './InitialDoubleSelect.module.css'
 import AviaoIcon from '../../assets/AviaoSVG.svg'
 import MotoIcon from '../../assets/MotoIcon.png'
+import { Link } from 'react-router-dom';
 const InitialDoubleSelect = () => {
     return (
         <div className={style.InitialDoubleSelect}>
             <div className={style.DivAroundSelects}>
+    <Link to="/delivery">
                 <div className={style.DivSelects}><div className={style.DivImg}><img src={MotoIcon}></img></div><p>Entrega de Documentos por todo o Brasil</p></div>
+
                 <div className={style.LineDoubleSelect}></div>
                 <div className={style.DivSelects}><div className={style.DivImg}><img src={AviaoIcon}></img></div><p>Entrega de Documentos Internacional</p></div>
+    </Link>        
             </div>
         </div>
     )
